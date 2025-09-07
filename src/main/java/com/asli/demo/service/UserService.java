505eb08a690a810fc41ecd5b59dc4ee024573752
Aslı Nur Mavili <aslinurmavili@gmail.com>
2025-09-08 00:52:08 +0300

    Save local changes before merge

--- src/main/java/com/asli/demo/service/UserService.java
+++ src/main/java/com/asli/demo/service/UserService.java
@@ -5,6 +5,7 @@ import com.asli.demo.entity.User;
 import java.util.List;
 import java.util.Optional;
 
+
 public interface UserService {
     User createUser(User user);
     Optional<User> getUserById(Long id);
