49bac9e3541753235022944e8481495d288725d2
Aslı Nur Mavili <aslinurmavili@gmail.com>
2025-09-09 23:05:36 +0300

    [SP-5] - Basit @RestController eklendi

--- src/main/java/com/asli/demo/controller/UserController.java
+++ src/main/java/com/asli/demo/controller/UserController.java
@@ -1,4 +1,49 @@
 package com.asli.demo.controller;
 
+import com.asli.demo.entity.User;
+import com.asli.demo.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    // CREATE
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    // READ
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    // READ
+    @GetMapping("/{id}")
+    public Optional<User> getUserById(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+
+    // UPDATE
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody User user) {
+        return userService.updateUser(id, user);
+    }
+
+    // DELETE
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+    }
 }
